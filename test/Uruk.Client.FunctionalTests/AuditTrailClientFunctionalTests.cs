2eb1fb95122fe2666ea3e5f50121424a48fe05cd
Yann CRUMEYROLLE <ycrumeyrolle@free.fr>
2020-03-30 00:13:24 +0200

    Avoid never ending functional tests (netcoreapp2.1 + IHost ?)

--- test/Uruk.Client.FunctionalTests/AuditTrailClientFunctionalTests.cs
+++ test/Uruk.Client.FunctionalTests/AuditTrailClientFunctionalTests.cs
@@ -175,7 +175,6 @@ namespace Uruk.Client.FunctionalTests
                 });
         }
 
-
         private class TestHttpMessageHandler : HttpMessageHandler
         {
             private readonly HttpResponseMessage? _response;
